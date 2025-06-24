c7df3a018cc1e6986fab1961a5e51cdccb899042
Francisco Duran <frandugo@ymail.com>
2025-06-23 21:09:02 -0500

    Add open sans font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const openSans = Open_Sans({ weight: ['700','500','400','300'], subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${openSans.className}`}>
         {children}
       </body>
     </html>
